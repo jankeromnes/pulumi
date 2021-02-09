1da396f6dde282388406eaf15c85eaf947f3adbb
Komal Ali <komal@pulumi.com>
2021-02-09 09:46:27 -0700

    python

--- sdk/python/lib/pulumi/x/automation/local_workspace.py
+++ sdk/python/lib/pulumi/x/automation/local_workspace.py
@@ -149,14 +149,12 @@ class LocalWorkspace(Workspace):
         return
 
     def get_config(self, stack_name: str, key: str) -> ConfigValue:
-        self.select_stack(stack_name)
-        result = self._run_pulumi_cmd_sync(["config", "get", key, "--json"])
+        result = self._run_pulumi_cmd_sync(["config", "get", key, "--json", "-s", stack_name])
         val = json.loads(result.stdout)
         return ConfigValue(**val)
 
     def get_all_config(self, stack_name: str) -> ConfigMap:
-        self.select_stack(stack_name)
-        result = self._run_pulumi_cmd_sync(["config", "--show-secrets", "--json"])
+        result = self._run_pulumi_cmd_sync(["config", "--show-secrets", "--json", "-s", stack_name])
         config_json = json.loads(result.stdout)
         config_map: ConfigMap = {}
         for key in config_json:
@@ -164,9 +162,8 @@ class LocalWorkspace(Workspace):
         return config_map
 
     def set_config(self, stack_name: str, key: str, value: ConfigValue) -> None:
-        self.select_stack(stack_name)
         secret_arg = "--secret" if value.secret else "--plaintext"
-        self._run_pulumi_cmd_sync(["config", "set", key, value.value, secret_arg])
+        self._run_pulumi_cmd_sync(["config", "set", key, value.value, secret_arg, "-s", stack_name])
 
     def set_all_config(self, stack_name: str, config: ConfigMap) -> None:
         # TODO: Do this in parallel after https://github.com/pulumi/pulumi/issues/6050
@@ -174,8 +171,7 @@ class LocalWorkspace(Workspace):
             self.set_config(stack_name, key, value)
 
     def remove_config(self, stack_name: str, key: str) -> None:
-        self.select_stack(stack_name)
-        self._run_pulumi_cmd_sync(["config", "rm", key])
+        self._run_pulumi_cmd_sync(["config", "rm", key, "-s", stack_name])
 
     def remove_all_config(self, stack_name: str, keys: List[str]) -> None:
         # TODO: Do this in parallel after https://github.com/pulumi/pulumi/issues/6050
@@ -183,8 +179,7 @@ class LocalWorkspace(Workspace):
             self.remove_config(stack_name, key)
 
     def refresh_config(self, stack_name: str) -> None:
-        self.select_stack(stack_name)
-        self._run_pulumi_cmd_sync(["config", "refresh", "--force"])
+        self._run_pulumi_cmd_sync(["config", "refresh", "--force", "-s", stack_name])
         self.get_all_config(stack_name)
 
     def who_am_i(self) -> WhoAmIResult:
@@ -257,17 +252,15 @@ class LocalWorkspace(Workspace):
         return plugin_list
 
     def export_stack(self, stack_name: str) -> Deployment:
-        self.select_stack(stack_name)
-        result = self._run_pulumi_cmd_sync(["stack", "export", "--show-secrets"])
+        result = self._run_pulumi_cmd_sync(["stack", "export", "--show-secrets", "-s", stack_name])
         state_json = json.loads(result.stdout)
         return Deployment(**state_json)
 
     def import_stack(self, stack_name: str, state: Deployment) -> None:
-        self.select_stack(stack_name)
         file = tempfile.NamedTemporaryFile(mode="w", delete=False)
         json.dump(state.__dict__, file, indent=4)
         file.close()
-        self._run_pulumi_cmd_sync(["stack", "import", "--file", file.name])
+        self._run_pulumi_cmd_sync(["stack", "import", "--file", file.name, "-s", stack_name])
         os.remove(file.name)
 
     def _run_pulumi_cmd_sync(self, args: List[str], on_output: Optional[OnOutput] = None) -> CommandResult:
